866e671677d282eeb9ef6080227581e1f0f208ff
Sean Collins <sean.collins@outlook.com>
2025-05-21 16:25:26 -0700

    Create basic Express server

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+// Initialize Express
+const app = express();
+
+// Define default server port
+const port = process.env.PORT || 3000;
+
+// Define application routes
+app.get('/', (req, res) => {
+  res.send('Entrypoint');
+});
+
+app.get('/api/data', (req, res) => {
+  // Simulate work
+  setTimeout(() => {
+    res.json({ message: 'Data from API', timestamp: new Date() });
+  }, 100);
+});
+
+app.listen(port, () => {
+  console.log(`Application listening on http://localhost:${port}`);
+});
